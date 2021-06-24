0a015d615afc30f5c7879b3e149d934d6075f72a
Dani2405 <daniel_240595@hotmail.com>
2021-06-24 03:13:07 +0200

    main gameplay

---
+++ lib/main.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import 'package:splitter_clicker/Game.dart';
+import 'package:splitter_clicker/Menu.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Splitter Clicker',
+      home: Menu(),
+      routes: {
+        '/play': (context) => Game(),
+      },
+    ),
+  );
+}
